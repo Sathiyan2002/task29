8ea645776feba1b3d1beed5b0187b8308f8f3930
sathiyan <sathiyan@gmail.com>
2024-01-29 08:18:27 +0000

    modified age

--- age.sh
+++ age.sh
@@ -8,8 +8,11 @@ then
 elif [ $a -ge 13 -a $a -le 19 ];
 then
         echo "Teenager"
-elif [ $a -ge 20 ];
+elif [ $a -ge 20 -a  $a -lt 60 ];
 then
 	echo "Adult"
+else [ $a -gt 60 ];
+
+        echo "oldage"
 fi
 
